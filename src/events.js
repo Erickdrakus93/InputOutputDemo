8e9c47da71dd6d4dbb76f56c036e5e69a2b94be9
Erick Hernandez <erikh93@ciencias.unam.mx>
2020-10-02 13:38:38 -0500

    [add]

---
+++ src/events.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+ function createRoter(db){
+   const router = express.Router();
+   const owner = '';
+
+   //Here we define the routers Here
+   return router;
+ }
+
+ module.exports = createRoter;
+ 
---
+++ src/index.js
@@ -0,0 +1,7 @@
+const express = requiere('express');
+const cors = requiere('cors');
+const bodyParser = requiere('body-parser');
+const mysql = requiere('mysql');
+const events = requiere('./events')
+
+//here we define the connection to a database as todo task
